88d39f101083a255b62ee314203f0b2a8a26a322
Shauncy5656 <shauncy5656@gmail.com>
2026-08-10 08:29:33 -1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,9 +3,9 @@ const CORE = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/apple-touch-icon.png'
+  'icon-192.png',
+  'icon-512.png',
+  'apple-touch-icon.png'
 ];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
